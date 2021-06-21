ae86c736ecaf6f35bd2166e6057690f377275140
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-06-21 15:19:17 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='susposter',
       author="Cryptex",
-      version="1.3.9",
+      version="1.4.3",
       description="so sussy",
       long_description=open("README.md").read()
       url="https://github.com/Cryptex-github/susposter",
