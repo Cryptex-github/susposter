4afa7f7b3ac816938ef4ea252fac54680fa47f3e
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-06-21 14:55:30 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,13 @@
-import re
 from setuptools import setup
-from setuptools_rust import Binding, RustExtension, Strip
-
-setup_requires = ['setuptools-rust>=0.9.2']
-install_requires = []
 
 setup(name='susposter',
       author="Cryptex",
-      version=version,
+      version="1.3.9,
       description="so sussy",
       long_description="sussy imposter,
       url="https://github.com/Cryptex-github/susposter",
-      rust_extensions=[
-          RustExtension('susposter.susposter', 'Cargo.toml', binding=Binding.PyO3, strip=Strip.Debug)],
-      setup_requires=setup_requires,
       include_package_data=True,
       packages=['susposter'],
-      zip_safe=False,
+      zip_safe=True,
       python_requires='>=3.6'
       )
