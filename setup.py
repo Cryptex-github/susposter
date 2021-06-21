9acfb4f840203cc19248069ea774f2b8afbf5944
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-06-21 16:02:18 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
+import re
 from setuptools import setup
+
+with open('susposter/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
 setup(name='susposter',
       author="Cryptex",
-      version="1.5.3",
+      version=version
       description="so sussy",
       long_description=open("README.md").read()
       url="https://github.com/Cryptex-github/susposter",
